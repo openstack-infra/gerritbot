21b5546061019516cab1ddbe46209f9b86ea843d
Clark Boylan <clark.boylan@gmail.com>
2012-08-15 13:48:55 -0700

    Remove Nova's description from setup.py.
    There was a copy pasta fail and Nova's project description was in
    GerritBot's setup.py. Replace that description with one for GerritBot.
    
    Change-Id: I92011d228cd35db93febb8eaefa5e576adeccfef

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setuptools.setup(
     version=version.canonical_version_string(always=True),
     author='Hewlett-Packard Development Company, L.P.',
     author_email='openstack@lists.launchpad.net',
-    description="Client library for OpenStack Nova API.",
+    description="Gerrit IRC Bot.",
     license="Apache License, Version 2.0",
     url="https://github.com/openstack-ci/gerritbot",
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
