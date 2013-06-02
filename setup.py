6c07bb6bc6333000da0a6604a22c86fbf90264c6
Monty Taylor <mordred@inaugust.com>
2013-06-02 08:32:22 -0400

    Migrate to pbr.
    Change-Id: Id9c9837b040c634103bcc30d590e995c3b6c309d

--- setup.py
+++ setup.py
@@ -1,59 +1,21 @@
-# Copyright 2011 OpenStack, LLC
-# Copyright 2012 Hewlett-Packard Development Company, L.P.
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import setuptools
 
-
-from gerritbot.openstack.common import setup
-from gerritbot.version import version_info as version
-
-requires = setup.parse_requirements()
-tests_require = setup.parse_requirements(['tools/test-requires'])
-depend_links = setup.parse_dependency_links()
-
-
-def read_file(file_name):
-    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
-
-
 setuptools.setup(
-    name="gerritbot",
-    version=version.canonical_version_string(always=True),
-    author='Hewlett-Packard Development Company, L.P.',
-    author_email='openstack@lists.launchpad.net',
-    description="Gerrit IRC Bot.",
-    license="Apache License, Version 2.0",
-    url="https://github.com/openstack-ci/gerritbot",
-    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-    include_package_data=True,
-    setup_requires=['setuptools_git>=0.4'],
-    cmdclass=setup.get_cmdclass(),
-    install_requires=requires,
-    dependency_links=depend_links,
-    tests_require=tests_require,
-    test_suite="nose.collector",
-    classifiers=[
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python"
-    ],
-    entry_points={
-        "console_scripts": ["gerritbot = gerritbot.bot:main"]
-    }
-)
+    setup_requires=['d2to1', 'pbr'],
+    d2to1=True)
